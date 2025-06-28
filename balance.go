9748a1711fdaf06adc5b7b6208d4d473c1d1c4b7
Acho Arnold <arnold@ndolestudio.com>
2025-06-28 22:37:24 +0300

    Extract balance response result into a struct

--- balance.go
+++ balance.go
@@ -8,14 +8,17 @@ import (
 
 // BalanceResponse is the response when querying the account balance
 type BalanceResponse struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
-	Result  struct {
-		AccountDepositNumber     string `json:"accountDepositNumber"`
-		AccountCommissionNumber  string `json:"accountCommissionNumber"`
-		AccountDepositBalance    int    `json:"accountDepositBalance"`
-		AccountCommissionBalance int    `json:"accountCommissionBalance"`
-	} `json:"result"`
+	Code    int                    `json:"code"`
+	Message string                 `json:"message"`
+	Result  *BalanceResponseResult `json:"result"`
+}
+
+// BalanceResponseResult contains the result of the balance response
+type BalanceResponseResult struct {
+	AccountDepositNumber     string `json:"accountDepositNumber"`
+	AccountCommissionNumber  string `json:"accountCommissionNumber"`
+	AccountDepositBalance    int    `json:"accountDepositBalance"`
+	AccountCommissionBalance int    `json:"accountCommissionBalance"`
 }
 
 // Balance lists all visible accounts of the given user, and for each account show the current deposit balance, and current commission.
